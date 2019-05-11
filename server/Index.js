70d4fb46fc36783fdb7d2746ccb66b9f06fdc861
sherlockDYing <16302010068@fudan.edu.cn>
2019-05-11 22:41:59 +0800

    redirect fix

--- server/Index.js
+++ server/Index.js
@@ -3,7 +3,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/',function (req,res) {
-    res.send('<h1>Hello world</h1>');
+    res.redirect('/index');
 });
 
 io.on('connection', function (socket) {
